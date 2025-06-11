ecba1dcdd39d60115d4a4d4bd2594863c60c156e
Rupali <rupalijondhale117@gmail.com>
2025-06-11 10:23:43 +0530

    Done

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 let app=require("./src/app.js");
-
-require('dotenv').config();
-
-app.listen(7777,()=>{
+let PORT=7777;
+app.listen(PORT,(req,res)=>{
 	
 	console.log("Server Started...");
 });
--- src/app.js
+++ src/app.js
@@ -1,24 +1,16 @@
 let express = require("express");
 let app = express();
-let cookieParser=require("cookie-parser");
+let cookieParser = require("cookie-parser");
 let cors = require("cors");
-let dotenv = require('dotenv');
+let dotenv = require("dotenv");
 
 let bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(cookieParser());
-
+let conn = require("../src/config/db");
 dotenv.config();
-module.exports=app;
-
-
-
-
-
-
- 
-
+module.exports = app;
