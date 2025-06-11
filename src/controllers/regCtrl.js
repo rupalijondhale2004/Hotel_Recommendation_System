ed21ff7c3d7c21d8eac8214df78fb3c47880b6ae
PRAJAKTANIKAM <prajktanikam0@gmail.com>
2025-06-11 11:08:32 +0530

    done

--- src/controllers/regCtrl.js
+++ src/controllers/regCtrl.js
@@ -1,3 +1,3 @@
 exports.homePage=(req,res)=>{
-	res.render("homepage.ejs");
+	res.render("homepage");
 }
--- src/routes/regrouts.js
+++ src/routes/regrouts.js
@@ -2,7 +2,7 @@ let routes=require("express");
 let regCtrl=require("../controllers/regCtrl");
 let router=routes.Router();
 
-router.post("/register",regCtrl.regCtrl);
+
 router.get("/",regCtrl.homePage);
 
 module.exports=router;
