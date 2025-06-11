ee54db2fda9c7fb80f7a79db35ff404599649f35
Rupali <rupalijondhale117@gmail.com>
2025-06-11 11:18:48 +0530

    Done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let app=require("./src/app.js");
-let PORT=7777;
+let PORT=8888;
 app.listen(PORT,(req,res)=>{
 	
 	console.log("Server Started...");
--- src/routes/regrouts.js
+++ src/routes/regrouts.js
@@ -2,7 +2,6 @@ let routes=require("express");
 let regCtrl=require("../controllers/regCtrl");
 let router=routes.Router();
 
-router.post("/register",regCtrl.regCtrl);
 router.get("/",regCtrl.homePage);
 
 module.exports=router;
