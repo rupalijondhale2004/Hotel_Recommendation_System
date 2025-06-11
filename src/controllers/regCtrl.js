2a2091138db078c473fc90163a67a1d60e9d296d
Rupali <rupalijondhale117@gmail.com>
2025-06-11 13:07:48 +0530

    Done this home

--- src/controllers/regCtrl.js
+++ src/controllers/regCtrl.js
@@ -1,25 +1,5 @@
-let regService=require("../services/regservice.js");
-let regmodel=require("../models/regmodel.js");
-
-exports.regCtrl=(req, res) => {
-	let {name,email,contact,username,password}=req.body;
-
-	
-	let result=regService.acceptRegData(name,email,contact,username,password);
-  	res.render("register",{msg:result});
-}
-
 exports.homePage=(req,res)=>{
 	res.render("homepage.ejs");
 }
 
 
-exports.signUpPage=(req,res)=>{
-
-	res.render("register.ejs",{msg:""});
-}
-
-exports.signInPage=(req,res)=>{
-
-	res.render("login.ejs",{msg:""});
-}
